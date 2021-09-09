ec30f8eb57c745598b12c9fea6b4ae84c3441b3b
freekgorrissen <f.gorrissen@gmail.com>
2021-09-09 09:34:59 +0200

    Oefenening 3 afgerond

--- oefening3.java
+++ oefening3.java
@@ -1,4 +1,20 @@
 
 public class oefening3 {
-
+    public static void main(String[] args) {
+    	int arr[] = {15, 17, 5, 4, 6, 15};
+    	
+    	int arr_sum1 = 0;
+    	for(int x = 0; x < arr.length; x ++) {
+    	arr_sum1 += arr[x];
+    	}
+    	
+    	System.out.println("De som van de array waarden is " + arr_sum1);
+    	
+    	int arr_sum2 = 0;
+    	for (int value : arr) {
+    		arr_sum2 += value;
+    	}
+    	System.out.println("De som van de array waarden is " + arr_sum2);
+    	
+    }
 }
